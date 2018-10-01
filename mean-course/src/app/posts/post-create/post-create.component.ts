339d0e625a139590733fb3ad20228db44208e367
Kyle Henry <khenry@swl.local>
2018-10-01 12:27:55 -0400

    Added angular material. Created post-list and header components .

---
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent {
+  enteredValue = '';
+  newPost = 'No Content';
+
+  onAddPost() {
+    this.newPost = this.enteredValue;
+  }
+}
---
+++ mean-course/src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list-component.css']
+})
+export class PostListComponent {
+
+  // posts = [
+  //   {title: 'First Post', content: 'First post content'},
+  //   {title: 'Second Post', content: 'Second post content'},
+  //   {title: 'Third Post', content: 'Third post content'}
+  // ];
+  posts = [];
+}
