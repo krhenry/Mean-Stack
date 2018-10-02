b72885225d228cd962fb1df0f20fe69eaed8ef3e
Kyle Henry <krhenry433@gmail.com>
2018-10-02 14:07:57 -0400

    Updated to have a post model and updated to proper form.

--- mean-course/src/app/app.component.ts
+++ mean-course/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 
+import { Post } from './posts/post.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // title = 'mean-course';
-  storedposts = [];
+  storedposts: Post[] = [];
 
   onPostAdded(post) {
     this.storedposts.push(post);
--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,8 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Post } from '../post.model';
+
 
 @Component({
   selector: 'app-post-create',
@@ -8,12 +12,15 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class PostCreateComponent {
   enteredTitle = '';
   enteredContent = '';
-  @Output() postCreated = new EventEmitter();
+  @Output() postCreated = new EventEmitter<Post>();
 
-  onAddPost() {
-    const post = {
-      title: this.enteredTitle,
-      content: this.enteredContent
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    const post: Post = {
+      title: form.value.title,
+      content: form.value.content
     };
     this.postCreated.emit(post);
   }
