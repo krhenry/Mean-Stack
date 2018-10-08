ee61d7be5043f8ee758f6821956c0948f573c161
Kyle Henry <krhenry433@gmail.com>
2018-10-08 10:58:17 -0400

    Added image to post. Need to update image on update. Start of L82.

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -16,6 +17,7 @@ mongoose.connect('mongodb+srv://kyle:bI4AHbH6cLiU6mhH@cluster0-tuzk9.mongodb.net
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/images', express.static(path.join('backend/images')));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
